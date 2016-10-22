8902e6c3bb1d53c7d5b36b776c4a71a927bc452e
Catherine French <catherine.french@me.com>
2016-10-22 19:57:47 -0400

    Updates datatype to JSONP.

--- linkedin.js
+++ linkedin.js
@@ -4,7 +4,7 @@ $(function(){
      url: 'http://q4modules.herokuapp.com/social/stream/linkedin',
      method: 'GET',
      data: '',
-     dataType: 'JSON'
+     dataType: 'JSONP'
    }).done(function(response){
         var socialJSON = response;
         var socialList = socialJSON["social"];
