e47f7e7f7bfc2ab21ae56aa97d68c8fb58e378b3
Catherine French <catherine.french@me.com>
2016-10-23 18:49:33 -0400

    Adds divs to Twitter feed for each json attribute.

--- linkedin.js
+++ linkedin.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
               // $('body').append(feed.favorites)
               // $('body').append($('<div>').text(feed.favorites))
 
-           $('body').append($('<br><div class = "fbfeed'  + i + '" > <br><br> ').text(feed.channel));
+           $('body').append($('<br><div class = "fb-feed'  + i + '" > <br><br> ').text(feed.channel));
            $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
            $('body').append($('<div class = "date'  + i + '" > ').text(feed.date));
            $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
@@ -32,7 +32,12 @@ $(document).ready(function() {
     }).done(function(responseData){
        console.log(responseData);
      responseData.forEach(function(feed){
-            $('body').append($('<div>').text(feed.title));
+            $('body').append($('<br><div class = "twitter-feed'  + i + '" > <br><br> ').text(feed.channel));
+              $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
+                $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
+                  $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
+                    $('body').append($('<div class = "user'  + i + '" > ').text(feed.user));
+
        });
      });
    });
