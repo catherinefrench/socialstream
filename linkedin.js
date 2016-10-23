c42f3e2c3c6ff119f40f2a604ebd349d13940785
Catherine French <catherine.french@me.com>
2016-10-23 12:11:13 -0400

    Makes click event that appends all Facebook titles to body.

--- linkedin.js
+++ linkedin.js
@@ -1,12 +1,18 @@
-$(function(){
- $('.social-stream').on('click', function(){
+$(document).ready(function() {
+ $('#facebook').click(function(){
    $.ajax({
      url: 'http://q4modules.herokuapp.com/social/stream/linkedin',
      method: 'GET',
-     data: '',
+     data: { },
      dataType: 'JSONP'
-   }).done(function(response){
-      console.log(response);
-      })
-    })
-  })
+   }).done(function(responseData){
+      console.log(responseData);
+    responseData.forEach(function(feed){
+              // $('body').append(feed.favorites)
+              // $('body').append($('<div>').text(feed.favorites))
+           $('body').append($('<div>').text(feed.title));
+      });
+    });
+
+  });
+    });
