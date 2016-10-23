42c5c05554f11eae6f7dac961e850e9c28789504
Catherine French <catherine.french@me.com>
2016-10-23 18:41:14 -0400

    Adds divs to fb feed json for each attribute.

--- linkedin.js
+++ linkedin.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+ i = 0
 
  $('#facebook').click(function(){
    $.ajax({
@@ -11,7 +12,13 @@ $(document).ready(function() {
     responseData.forEach(function(feed){
               // $('body').append(feed.favorites)
               // $('body').append($('<div>').text(feed.favorites))
-           $('body').append($('<div>').text(feed.title));
+
+           $('body').append($('<br><div class = "fbfeed'  + i + '" > <br><br> ').text(feed.channel));
+           $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
+           $('body').append($('<div class = "date'  + i + '" > ').text(feed.date));
+           $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
+           $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
+           i = i + 1
       });
     });
   });
