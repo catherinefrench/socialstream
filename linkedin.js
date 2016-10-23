a36f4f90d819fe3f1df54235a73a47dab3776b5a
Catherine French <catherine.french@me.com>
2016-10-23 12:45:39 -0400

    Adds Twitter and Linkedin buttons with click event and handler to append titles to body.

--- linkedin.js
+++ linkedin.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+
  $('#facebook').click(function(){
    $.ajax({
-     url: 'http://q4modules.herokuapp.com/social/stream/linkedin',
+     url: 'http://q4modules.herokuapp.com/social/stream/linkedin/facebook',
      method: 'GET',
      data: { },
      dataType: 'JSONP'
@@ -13,6 +14,36 @@ $(document).ready(function() {
            $('body').append($('<div>').text(feed.title));
       });
     });
-
   });
+
+  $('#twitter').click(function(){
+    $.ajax({
+      url: 'http://q4modules.herokuapp.com/social/stream/linkedin/twitter',
+      method: 'GET',
+      data: { },
+      dataType: 'JSONP'
+    }).done(function(responseData){
+       console.log(responseData);
+     responseData.forEach(function(feed){
+            $('body').append($('<div>').text(feed.title));
+       });
+     });
+   });
+
+   $('#youtube').click(function(){
+     $.ajax({
+       url: 'http://q4modules.herokuapp.com/social/stream/linkedin/youtube',
+       method: 'GET',
+       data: { },
+       dataType: 'JSONP'
+     }).done(function(responseData){
+        console.log(responseData);
+      responseData.forEach(function(feed){
+             $('body').append($('<div>').text(feed.title));
+        });
+      });
+    });
+
+
+
     });
