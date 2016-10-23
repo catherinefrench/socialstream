d885832cb150d13d3de0b5b644f9b340d1756b2b
Catherine French <catherine.french@me.com>
2016-10-23 19:20:41 -0400

    Adds comments to AJAX and jQuery.

--- linkedin.js
+++ linkedin.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function() { //ready's document
+
  i = 0
 
- $('#facebook').click(function(){
+ $('#facebook').click(function(){ //ajax request for Facebook feed
    $.ajax({
      url: 'http://q4modules.herokuapp.com/social/stream/linkedin/facebook',
      method: 'GET',
      data: { },
      dataType: 'JSONP'
-   }).done(function(responseData){
+   }).done(function(responseData){ //handles responseData upon completion of click event
       console.log(responseData);
-    responseData.forEach(function(feed){
-              // $('body').append(feed.favorites)
-              // $('body').append($('<div>').text(feed.favorites))
+    responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
 
-           $('body').append($('<br><div class = "fb-feed'  + i + '" > <br><br> ').text(feed.channel));
+           $('body').append($('<br><div class = "fb-feed'  + i + '" > <br>').text(feed.channel));
            $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
            $('body').append($('<div class = "date'  + i + '" > ').text(feed.date));
            $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
@@ -23,7 +22,7 @@ $(document).ready(function() {
     });
   });
 
-  $('#twitter').click(function(){
+  $('#twitter').click(function(){ //ajax request for Twitter feed
     $.ajax({
       url: 'http://q4modules.herokuapp.com/social/stream/linkedin/twitter',
       method: 'GET',
@@ -31,18 +30,18 @@ $(document).ready(function() {
       dataType: 'JSONP'
     }).done(function(responseData){
        console.log(responseData);
-     responseData.forEach(function(feed){
-            $('body').append($('<br><div class = "twitter-feed'  + i + '" > <br><br> ').text(feed.channel));
-              $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
-                $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
-                  $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
-                    $('body').append($('<div class = "user'  + i + '" > ').text(feed.user));
+     responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
+            $('body').append($('<br><div class = "twitter-feed'  + i + '" > <br>').text(feed.channel));
+            $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
+            $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
+            $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
+            $('body').append($('<div class = "user'  + i + '" > ').text(feed.user));
 
        });
      });
    });
 
-   $('#youtube').click(function(){
+   $('#youtube').click(function(){ //ajax request for YouTube feed
      $.ajax({
        url: 'http://q4modules.herokuapp.com/social/stream/linkedin/youtube',
        method: 'GET',
@@ -50,8 +49,12 @@ $(document).ready(function() {
        dataType: 'JSONP'
      }).done(function(responseData){
         console.log(responseData);
-      responseData.forEach(function(feed){
-             $('body').append($('<div>').text(feed.title));
+      responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
+              $('body').append($('<br><div class = "twitter-feed'  + i + '" > <br>').text(feed.channel));
+              $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
+              $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
+              $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
+              $('body').append($('<div class = "thumbnails'  + i + '" > ').text(feed.user));
         });
       });
     });
