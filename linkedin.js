13b7c26691b8e1279fbc7be8a92c72ad251314b3
Catherine French <catherine.french@me.com>
2016-10-24 00:34:36 -0400

    Fixes Twitter div names.

--- linkedin.js
+++ linkedin.js
@@ -32,17 +32,18 @@ $(document).ready(function() { //ready's document
        console.log(responseData);
      responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
       //  var responseJSON = responseData;
-            $('body').append($('<br><div class = "twitter-feed'  + i + '" > <br>').text(feed.channel));
-            $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
-            $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
-            $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
-            $('body').append($('<div class = "link'  + i + '" > ').text(feed.favorites));
-            $('body').append($('<div class = "link'  + i + '" > ').text(feed.retweets));
-            // $('body').append($('<div class = "user'  + i + '" > ').text(feed.user));
-            $('body').append($('<div>').text(feed.user.id));
-            $('body').append($('<div>').text(feed.user.name));
-            $('body').append($('<div>').text(feed.user.screen_name));
-            $('body').append($('<div>').text(feed.user.image));
+
+          $('body').append($('<br><div class = "twitter-feed">').text(feed.channel));
+           $('body').append($('<div class = "twitter-title">').text(feed.title));
+           $('body').append($('<div class = "twitter-from_now">').text(feed.from_now));
+           $('body').append($('<div class = "twitter-link">').text(feed.link));
+           $('body').append($('<div class = "twitter-favorites">').text(feed.favorites));
+           $('body').append($('<div class = "twitter-retweets">').text(feed.retweets));
+           // $('body').append($('<div class = "user'  + i + '" > ').text(feed.user));
+           $('body').append($('<div class = "twitter-user-id">').text(feed.user.id)); //appends to id in user hash in feed hash
+           $('body').append($('<div class = "twitter-user-name">').text(feed.user.name));
+           $('body').append($('<div class = "twitter-user-screen_name">').text(feed.user.screen_name));
+           $('body').append($('<div class = "twitter-user-image">').text(feed.user.image));
 
        });
      });
