c234e376ce7e79ac0ecfb0777268c06ce8973eb3
Catherine French <catherine.french@me.com>
2016-10-24 00:43:17 -0400

    Fixes youtube div class names and adds the three thumbnails attributes with .append.

--- linkedin.js
+++ linkedin.js
@@ -58,11 +58,13 @@ $(document).ready(function() { //ready's document
      }).done(function(responseData){
         console.log(responseData);
       responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
-              $('body').append($('<br><div class = "twitter-feed'  + i + '" > <br>').text(feed.channel));
-              $('body').append($('<div class = "title'  + i + '" > ').text(feed.title));
-              $('body').append($('<div class = "from_now'  + i + '" > ').text(feed.from_now));
-              $('body').append($('<div class = "link'  + i + '" > ').text(feed.link));
-              $('body').append($('<div class = "thumbnails'  + i + '" > ').text(feed.user));
+              $('body').append($('<br><div class = "youtube-twitter-feed" > <br>').text(feed.channel));
+              $('body').append($('<div class = "youtube-title" > ').text(feed.title));
+              $('body').append($('<div class = "youtube-from_now" > ').text(feed.from_now));
+              $('body').append($('<div class = "youtube-link" > ').text(feed.link));
+              $('body').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.default));
+              $('body').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.medium));
+              $('body').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.high));
         });
       });
     });
