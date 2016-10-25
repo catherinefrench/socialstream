4a82b91f2f6cc4c1a449ac2c91c9026705dbc30b
Catherine French <catherine.french@me.com>
2016-10-25 01:57:21 -0400

    Changes stream names to specify what social site it's from.

--- linkedin.js
+++ linkedin.js
@@ -11,11 +11,11 @@ $(document).ready(function() { //ready's document
       console.log(responseData);
     responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
 
-           $('#social-stream-feed').append($('<br><div class = "fb-feed" > <br>').text(feed.channel));
-           $('#social-stream-feed').append($('<div class = "fb-from_now" > ').text(feed.from_now));
-           $('#social-stream-feed').append($('<div class = "fb-date" > ').text(feed.date));
-           $('#social-stream-feed').append($('<div class = "fb-title" > ').text(feed.title));
-           $('#social-stream-feed').append($('<div class = "fb-link" > ').text(feed.link));
+           $('#facebook-stream-feed').append($('<br><div class = "fb-feed" > <br>').text(feed.channel));
+           $('#facebook-stream-feed').append($('<div class = "fb-from_now" > ').text(feed.from_now));
+           $('#facebook-stream-feed').append($('<div class = "fb-date" > ').text(feed.date));
+           $('#facebook-stream-feed').append($('<div class = "fb-title" > ').text(feed.title));
+           $('#facebook-stream-feed').append($('<div class = "fb-link" > ').text(feed.link));
 
       });
     });
@@ -32,16 +32,16 @@ $(document).ready(function() { //ready's document
      responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
       //  var responseJSON = responseData;
 
-          $('#social-stream-feed').append($('<br><div class = "twitter-feed">').text(feed.channel));
-           $('#social-stream-feed').append($('<div class = "twitter-title">').text(feed.title));
-           $('#social-stream-feed').append($('<div class = "twitter-from_now">').text(feed.from_now));
-           $('#social-stream-feed').append($('<div class = "twitter-link">').text(feed.link));
-           $('#social-stream-feed').append($('<div class = "twitter-favorites">').text(feed.favorites));
-           $('#social-stream-feed').append($('<div class = "twitter-retweets">').text(feed.retweets));
-           $('#social-stream-feed').append($('<div class = "twitter-user-id">').text(feed.user.id)); //appends to id in user hash in feed hash
-           $('#social-stream-feed').append($('<div class = "twitter-user-name">').text(feed.user.name));
-           $('#social-stream-feed').append($('<div class = "twitter-user-screen_name">').text(feed.user.screen_name));
-           $('#social-stream-feed').append($('<div class = "twitter-user-image">').text(feed.user.image));
+          $('#twitter-stream-feed').append($('<br><div class = "twitter-feed">').text(feed.channel));
+           $('#twitter-stream-feed').append($('<div class = "twitter-title">').text(feed.title));
+           $('#twitter-stream-feed').append($('<div class = "twitter-from_now">').text(feed.from_now));
+           $('#twitter-stream-feed').append($('<div class = "twitter-link">').text(feed.link));
+           $('#twitter-stream-feed').append($('<div class = "twitter-favorites">').text(feed.favorites));
+           $('#twitter-stream-feed').append($('<div class = "twitter-retweets">').text(feed.retweets));
+           $('#twitter-stream-feed').append($('<div class = "twitter-user-id">').text(feed.user.id)); //appends to id in user hash in feed hash
+           $('#twitter-stream-feed').append($('<div class = "twitter-user-name">').text(feed.user.name));
+           $('#twitter-stream-feed').append($('<div class = "twitter-user-screen_name">').text(feed.user.screen_name));
+           $('#twitter-stream-feed').append($('<div class = "twitter-user-image">').text(feed.user.image));
 
 
        });
@@ -58,15 +58,15 @@ $(document).ready(function() { //ready's document
         console.log(responseData);
       responseData.forEach(function(feed){ //For each object, appends its attributes to body and adds class name
               // $('#social-stream-feed').append($('<br><div class = "youtube-feed" > <br>').text(feed.channel));
-              $('#social-stream-feed').append($('<div class = "youtube-title" > ').text(feed.title));
-              $('#social-stream-feed').append($('<div class = "youtube-from_now" > ').text(feed.from_now));
+              $('#youtube-stream-feed').append($('<div class = "youtube-title" > ').text(feed.title));
+              $('#youtube-stream-feed').append($('<div class = "youtube-from_now" > ').text(feed.from_now));
               // $('#social-stream-feed').append($('<div class = "youtube-link" > ').text(feed.link));
               var youtubeLink = ('<iframe src="' + feed.link + '&output=embed' + '"></iframe>');
               console.log('ED IS AWESOME');
-              $('#social-stream-feed').append(youtubeLink);
-              $('#social-stream-feed').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.default));
-              $('#social-stream-feed').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.medium));
-              $('#social-stream-feed').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.high));
+              $('#youtube-stream-feed').append(youtubeLink);
+              $('#youtube-stream-feed').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.default));
+              $('#youtube-stream-feed').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.medium));
+              $('#youtube-stream-feed').append($('<div class = "youtube-thumbnails" > ').text(feed.thumbnails.high));
         });
       });
     });
